cf3542cfe4a8287cb2fc2c4795ae0fd9ab5d1483
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-08-11 15:00:56 +0100

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,19 +26,19 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', strict_slashes=False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
-    """ GET /api/v1/unauthorized[/]
-    Returns:
-      - an unauthorized error message
+    """ GET /api/v1/unauthorized
+    Return:
+      - raises a 401 error by using abort
     """
     abort(401)
 
 
-@app_views.route('/forbidden', strict_slashes=False)
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
-    """ GET /api/v1/forbidden[/]
-    Returns:
-      - a forbidden error message
+    """ GET /api/v1/forbidden
+    Return:
+      - raises a 403 error by using abort
     """
     abort(403)
