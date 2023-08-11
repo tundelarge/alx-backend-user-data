2def27fe6284bf802a9cd6960181960187b4186b
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-08-11 14:55:50 +0100

    Update index.py

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -14,24 +14,6 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
-    Return:
-      - abort(401)
-    """
-    return abort(401)
-
-
-@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
-def forbidden() -> str:
-    """ GET /api/v1/forbidden
-    Return:
-      - abort(403)
-    """
-    return abort(403)
-
-
 @app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
@@ -42,3 +24,21 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized[/]
+    Returns:
+      - an unauthorized error message
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden[/]
+    Returns:
+      - a forbidden error message
+    """
+    abort(403)
